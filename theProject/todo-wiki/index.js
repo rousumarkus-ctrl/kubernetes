cb93ecddc0980b4b6456333422bd18c0c8610cde
Markus Rousu <rousu.markus@gmail.com>
2025-12-13 23:31:07 +0200

    wiki

--- theProject/todo-wiki/index.js
+++ theProject/todo-wiki/index.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 
 const makeLink = async () => {
-  const link = await axios.get(process.env.WIKI_URL);
-  console.log(link);
-  const result = await axios.post(process.env.TODO_URL, { title: link });
-  console.log(result);
+  const response = await axios.get(process.env.WIKI_URL, {
+    headers: {
+      'User-Agent': 'curl/7.79.1',
+    },
+    maxRedirects: 0,
+    validateStatus: (status) => status === 302,
+  });
+  const result = await axios.post(process.env.TODO_URL, {
+    title: `Read ${response.headers.location}`,
+  });
 };
 makeLink();
