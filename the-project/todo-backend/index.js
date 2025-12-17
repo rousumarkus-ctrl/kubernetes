05800303a20078835d3be4b8c4297f5e9309079b
Markus Rousu <rousu.markus@gmail.com>
2025-12-17 03:19:13 +0200

    new branch

--- the-project/todo-backend/index.js
+++ the-project/todo-backend/index.js
@@ -5,16 +5,21 @@ import pool from './db.js';
 
 const directory = path.join('/', 'usr', 'src', 'app', 'images');
 
-const PORT = process.env.BACKEND_PORT;
-app.listen(PORT, async () => {
-  console.log('Got to query');
+const createTable = async () => {
   try {
     const res = await pool.query(
       `CREATE TABLE IF NOT EXISTS todos (id SERIAL PRIMARY KEY,title TEXT NOT NULL);`
     );
   } catch (e) {
     console.log('Query failed', e);
+    setTimeout(createTable, 5000);
   }
+};
+
+const PORT = process.env.BACKEND_PORT;
+app.listen(PORT, async () => {
+  console.log('Got to query');
+  createTable();
 
   fs.promises.mkdir(directory, { recursive: true });
   console.log(`Server started in port ${PORT}`);
